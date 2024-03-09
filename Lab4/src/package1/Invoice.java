c14d9e8f612e3371becde3e2a1439f5d93f591c6
Isaiah Iruoha <Siruoha@gmail.com>
2024-03-09 17:48:23 -0500

    Lab 4 Complete

---
+++ Lab4/src/package1/Invoice.java
@@ -0,0 +1,71 @@
+package package1;
+
+public class Invoice implements Comparable<Invoice>, PayAble{
+	
+	private String companyName;
+	private Double amount;
+	
+	public Invoice(){
+		companyName = "No Name";
+		amount = 0.0;
+	}
+	
+	public Invoice(String theName, Double theAmount){
+		companyName = theName;
+		if (theName == null || theAmount < 0)
+        {
+             System.out.println("Fatal Error creating Invoice.");
+             System.exit(0);
+        }
+        companyName = theName;
+        amount = theAmount;
+	}
+	
+	public int compareTo(Invoice bill) {
+	    if (this.amount < bill.amount) return -1;
+	    else if (this.amount > bill.amount) return 1;
+	    else return 0;
+	}
+	
+	public Double amountToPay() {
+	    return this.amount;
+	}
+	
+	public void printPayment() {
+	    System.out.println("Payment information for an invoice. Company name: " + this.companyName + "; payment " + amountToPay() + ".");
+	}
+	
+	public String getName(){
+		return companyName;
+	}
+	
+	public Double getAmount(){
+		return amount;
+	}
+	
+	public void setName(String newName){
+		if (newName == null)
+        {
+             System.out.println("Fatal Error setting invoice name.");
+             System.exit(0);
+        }
+       else
+            companyName = newName;
+	}
+	
+	public void setAmount(Double newAmount){
+		if (newAmount < 0)
+        {
+             System.out.println("Fatal Error setting invoice name.");
+             System.exit(0);
+        }
+       else
+            amount = newAmount;
+	}
+	
+	public String toString(){
+		return companyName + " is owed " + amount;
+	}
+	
+
+}
---
+++ Lab4/src/package1/PayAble.java
@@ -0,0 +1,9 @@
+package package1;
+
+public interface PayAble {
+	
+	public Double amountToPay();
+	
+	public void printPayment(); 
+
+}
